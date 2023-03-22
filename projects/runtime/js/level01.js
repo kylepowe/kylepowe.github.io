e7a9951fd3dbcf6435513cd0bbdd8b9c60fd4eda
kylepowe <kp1249992@gmail.com>
2023-03-22 18:48:29 +0000

    push

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -27,10 +27,31 @@ var level01 = function (window) {
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
-
-        
-        
-        
+        function createSawBlade(x, y){
+            var hitZoneSize = 25; // declares var hitZoneSize and assigns it to the value 25
+            var damageFromObstacle = 10; // declares var damageFromObsticle and assigns it to the value 10
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); // declares var sawBladeHitZone and creates a sawblade obsticle
+            sawBladeHitZone.x = x; // assigns a value to sawBladeHitZon.x which is now 400 pixels
+            sawBladeHitZone.y = y; // assigns a value to sawBladeHitZone which is now 100 pixels
+            game.addGameItem(sawBladeHitZone); // creates the sawblade and exicutes the code putting it on screen
+            var obstacleImage = draw.bitmap("img/sawblade.png"); // adds in the image of the sawblade itself
+            sawBladeHitZone.addChild(obstacleImage); // adds the sawblade as a childl child to the background
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+        }
+        createSpike(400, 400);
+        function createSpike(x, y){
+            var hitZoneSize = 25; // declares var hitZoneSize and assigns it to the value 25
+            var damageFromObstacle = 10; // declares var damageFromObsticle and assigns it to the value 10
+            var spikeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); // declares var sawBladeHitZone and creates a sawblade obsticle
+            spikeHitZone.x = x; // assigns a value to sawBladeHitZon.x which is now 400 pixels
+            spikeHitZone.y = y; // assigns a value to sawBladeHitZone which is now 100 pixels
+            game.addGameItem(spikeHitZone); // creates the sawblade and exicutes the code putting it on screen
+            var obstacleImage = draw.bitmap("img/sawblade.png"); // adds in the image of the sawblade itself
+            spikeHitZone.addChild(obstacleImage); // adds the sawblade as a childl child to the background
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+        }
         // DO NOT EDIT CODE BELOW HERE
     }
 };
